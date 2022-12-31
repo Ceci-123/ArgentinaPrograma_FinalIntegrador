a97ce3b33bb3f8a296ade62d226589afcbceb153
Ceci <78119165+Ceci-123@users.noreply.github.com>
2022-12-31 19:47:31 -0300

    falta atributo de clases

--- scripts.js
+++ scripts.js
@@ -7,28 +7,35 @@ const experiencia = document.getElementById("experiencia");
 window.addEventListener("click", (e) => {
   let emisor = e.path[0];
   let punteroContenido;
+  let punteroTitulitos;
   switch (emisor) {
     case nombre:
       punteroContenido = document.getElementsByClassName("contenidos")[0];
+      punteroTitulitos = document.getElementsByClassName("titulitos")[0];
       console.log(punteroContenido);
-      x.removeAttribute("display");
-      x.setAttribute("display", "block");
+      console.log(punteroTitulitos);
+      punteroTitulitos.setAttribute("display", "none");
+      punteroContenido.setAttribute("display", "block");
 
       break;
     case contacto:
       punteroContenido = document.getElementsByClassName("contenidos")[1];
+      punteroTitulitos = document.getElementsByClassName("titulitos")[1];
       console.log(punteroContenido);
       break;
     case manifiesto:
       punteroContenido = document.getElementsByClassName("contenidos")[2];
+      punteroTitulitos = document.getElementsByClassName("titulitos")[2];
       console.log(punteroContenido);
       break;
     case estudios:
       punteroContenido = document.getElementsByClassName("contenidos")[3];
+      punteroTitulitos = document.getElementsByClassName("titulitos")[3];
       console.log(punteroContenido);
       break;
     case experiencia:
       punteroContenido = document.getElementsByClassName("contenidos")[4];
+      punteroTitulitos = document.getElementsByClassName("titulitos")[4];
       console.log(punteroContenido);
       break;
     default:
