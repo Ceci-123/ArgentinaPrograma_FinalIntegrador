1dc9d0a9c405ad4b41fdc23c22969ea03ee21572
Ceci <78119165+Ceci-123@users.noreply.github.com>
2023-01-01 14:42:47 -0300

    falta reemplazar contenido

--- scripts.js
+++ scripts.js
@@ -14,29 +14,30 @@ window.addEventListener("click", (e) => {
       punteroTitulitos = document.getElementsByClassName("titulitos")[0];
       console.log(punteroContenido);
       console.log(punteroTitulitos);
-      punteroTitulitos.setAttribute("display", "none");
-      punteroContenido.setAttribute("display", "block");
-
       break;
     case contacto:
       punteroContenido = document.getElementsByClassName("contenidos")[1];
       punteroTitulitos = document.getElementsByClassName("titulitos")[1];
       console.log(punteroContenido);
+      console.log(punteroTitulitos);
       break;
     case manifiesto:
       punteroContenido = document.getElementsByClassName("contenidos")[2];
       punteroTitulitos = document.getElementsByClassName("titulitos")[2];
       console.log(punteroContenido);
+      console.log(punteroTitulitos);
       break;
     case estudios:
       punteroContenido = document.getElementsByClassName("contenidos")[3];
       punteroTitulitos = document.getElementsByClassName("titulitos")[3];
       console.log(punteroContenido);
+      console.log(punteroTitulitos);
       break;
     case experiencia:
       punteroContenido = document.getElementsByClassName("contenidos")[4];
       punteroTitulitos = document.getElementsByClassName("titulitos")[4];
       console.log(punteroContenido);
+      console.log(punteroTitulitos);
       break;
     default:
       alert("otra opcion");
